a0121a137955bc175b125476400617db49748c70
riya_dongre <dongreriya48@gmail.com>
2024-03-08 01:33:17 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import React from react;
+import Login from Login; 
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const App=()=>{
+	return(
+		<div>
+		  <Login/>
+		</div>
+	)
 }
+
+export default App 
---
+++ src/pages/login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login=()=>{
+	return(
+		<div>
+			<form action=""> 
+				<div> 
+					<label htmlFor="email">Email</label>
+					<input type="text" name="email" id="email"/> 
+				</div> 
+				<div> 
+					<label htmlFor="passw">Password</label>
+					<input type="text" name="passw" id="password"/> 
+				</div>  
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	)
+}
+export default Login  
